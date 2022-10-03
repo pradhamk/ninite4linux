f4f972a53808b151fe3c0e3a14aa39e52e5c63ab
Pradham K <email@pradhamk.com>
2022-10-02 23:30:57 -0400

    random test data with reading in json

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 )
 
+type Packages struct {
+	Packages []Package `json: "packages"`
+}
+
+type Package struct {
+	Name string `json: "name"`
+}
+
 func main() {
-	fmt.Println("initial commit")
+	file, err := os.Open("packages.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+	FileData, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	var packages Packages
+	json.Unmarshal(FileData, &packages)
 }
