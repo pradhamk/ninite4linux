fe26d3a48070e10897a19766f60ccc02ab2cf570
Pradham K <email@pradhamk.com>
2022-10-03 00:13:50 -0400

    add more to base packages.json and similar formatting in go code

--- main.go
+++ main.go
@@ -12,19 +12,33 @@ type Packages struct {
 }
 
 type Package struct {
-	Name string `json: "name"`
+	Name        string     `json: "name"`
+	Description string     `json: "description"`
+	Platforms   []Platform `json: "platforms"`
+}
+
+type Platform struct {
+	Name     string   `json: "name"`
+	Commands []string `json: "commands"`
+}
+
+func ErrHandle(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func ReadPackagesList() Packages {
+	var packages Packages
+	file, err := os.Open("packages.json")
+	ErrHandle(err)
+	defer file.Close()
+	FileData, err := ioutil.ReadAll(file)
+	ErrHandle(err)
+	json.Unmarshal(FileData, &packages)
+	return packages
 }
 
 func main() {
-	file, err := os.Open("packages.json")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer file.Close()
-	FileData, err := ioutil.ReadAll(file)
-	if err != nil {
-		fmt.Println(err)
-	}
-	var packages Packages
-	json.Unmarshal(FileData, &packages)
+	packages := ReadPackagesList()
 }
